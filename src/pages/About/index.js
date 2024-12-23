d5b145f7525945f892ed8d48554f111db7a088eb
Brian <blascuna@gmail.com>
2024-12-23 13:05:19 -0800

    Updated About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -14,11 +14,12 @@ const About = () => {
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">About Me</h5>
                         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                            Hi, my name is Brian Lascuna and I'm an aspiring programmer and full stack developer striving to learn more within the industry and challenge myself, whether it be develpment technologies 
+                            Hi, my name is Brian Lascuna and I'm an aspiring programmer striving to learn more within the industry and challenge myself, whether it be develpment technologies 
                             or techniques within development. I've been studying Computer Science for almost 5 years and currently am proficient in C++, Python, and Javascript. I obtained a Certificate of Completion 
                             for Full Stack Web Development from a certificate program accredited by UC Davis and through this I have also picked up proficiencies in web development technologies such as MySQL, Mongo, 
                             Express, and React among others. I have excellent critical thinking skills to help tackle difficult situations and overcome any challenge during development and have confidence in my ability 
-                            to quickly learn and master new skills. I'm an all around team player, willing to adapt to any role to ensure the project is successful.
+                            to quickly learn and master new skills. I'm an all around team player, willing to adapt to any role to ensure the project is successful. This portfolio documents my experiences with full stack development
+                            and is currently outdated.
                         </p>
                         <a href="https://www.credly.com/badges/1d5faea6-3210-4186-9f56-8538a12bc814/public_url" target="_blank" className="course-badge"><img src={badge} alt="course-badge-pic" /></a>
                     </div>
